16d40025ae8ab8b290ab34d2d85b7a3ccb84793f
Florian Roessler <roessler.fd@gmail.com>
2021-07-18 23:06:18 +0200

    max 5 replacements

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     )
     readme = Path(os.environ.get("myReadme", "readme.md"))
     text = readme.read_text()
-    text2 = full_match.sub(repl_starcount, text)
+    text2 = full_match.sub(repl_starcount, text, count=5)
     readme.write_text(text2)
 
 
