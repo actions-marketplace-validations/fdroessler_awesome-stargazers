0b343b3a207b14039f00a67fa305f81b23ada339
Florian Roessler <roessler.fd@gmail.com>
2021-07-18 23:08:47 +0200

    flexible replacements

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ def main():
     )
     readme = Path(os.environ.get("myReadme", "readme.md"))
     text = readme.read_text()
-    text2 = full_match.sub(repl_starcount, text, count=5)
+    text2 = full_match.sub(
+        repl_starcount, text, count=int(os.environ.get("NUM_REPLACEMENTS", 0))
+    )
     readme.write_text(text2)
 
 
