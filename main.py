013a1f3dd85f3457bc40e21ad33639090de91c47
Florian Roessler <roessler.fd@gmail.com>
2021-07-18 10:30:05 +0200

    first version to test

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import re
+from pathlib import Path
+from github import Github
+
+g = Github()
+
+
+def repl_starcount(match):
+    repo = g.get_repo(f"{match.group('company')}/{match.group('repo')}")
+    count = repo.stargazers_count
+    if len(match.group("star")) > 0:
+        return re.sub(f":star:{match.group('star')}", f":star:{count}", match.group(0))
+    return f"[{match.group('text')} :star:{count}]({match.group('link')})"
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
+    full_match = re.compile(
+        r"\[(?P<text>.*?(:star:)?(?P<star>\d*))]\((?P<link>https?://github.com/(?P<company>[^/\n]*)/(?P<repo>[^/\n)]*).*)\)"
+    )
+    readme = Path(os.environ.get("myReadme", "readme.md"))
+    text = readme.read_text()
+    text2 = full_match.sub(repl_starcount, text)
+    readme.write_text(text2)
 
 
 if __name__ == "__main__":
